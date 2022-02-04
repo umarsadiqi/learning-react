b78ecff63e0fcb8445698750333c4ba1d20768fd
umarsadiqi <82892724+umarsadiqi@users.noreply.github.com>
2022-02-04 09:18:29 +0500

    Created class-based Component
    ⨀ created App class-based component
    ⨀ class must extends React.Component
    ⨀ called render() method

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    return (
-        <div>App</div>
-    );
-};
+class App extends React.Component {
+    // we have to define render() method in every class-based component
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+
+        return (
+            <div>Latitude: </div>
+        )
+    }
+
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
