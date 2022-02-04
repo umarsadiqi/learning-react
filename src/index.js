c8b986cfe0759ad62cc43e9bb49c19957ebf9e99
umarsadiqi <82892724+umarsadiqi@users.noreply.github.com>
2022-02-04 09:23:42 +0500

    initialized & updated state
    ⨀ called constructor() method
    ⨀ initialized state by this.state = { ... }
    ⨀ updated state by this.setState({ ... })
    ⨀ moved getCurrentPosition() method to constructor

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    // we have to define render() method in every class-based component
-    render() {
+    constructor(props) {
+        // must call super() in constructor
+        super(props);
+
+        // this is only time, we direct assign object to this.state
+        // elseWhere, we use setState()
+        this.state = { lat: null };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => this.setState({ lat: position.coords.latitude }),
             (err) => console.log(err)
         );
+    }
 
+    // we have to define render() method in every class-based component
+    render() {
         return (
-            <div>Latitude: </div>
+            <div>Latitude: {this.state.lat}</div>
         )
     }
 
 }
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
