d8638587e3d963c0cf15b300fa9a429a0feba347
umarsadiqi <82892724+umarsadiqi@users.noreply.github.com>
2022-02-07 08:34:25 +0500

    Revert "initialized & updated state"
    This reverts commit c8b986cfe0759ad62cc43e9bb49c19957ebf9e99.

--- src/index.js
+++ src/index.js
@@ -2,28 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    constructor(props) {
-        // must call super() in constructor
-        super(props);
-
-        // this is only time, we direct assign object to this.state
-        // elseWhere, we use setState()
-        this.state = { lat: null };
-
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude }),
-            (err) => console.log(err)
-        );
-    }
-
     // we have to define render() method in every class-based component
     render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+
         return (
-            <div>Latitude: {this.state.lat}</div>
+            <div>Latitude: </div>
         )
     }
 
 }
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
