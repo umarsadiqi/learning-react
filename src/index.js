6edbd39af72ac2bbd844f50a3a0327d22a38db27
umarsadiqi <82892724+umarsadiqi@users.noreply.github.com>
2022-02-07 08:34:25 +0500

    Revert "Refactor the position of state"
    This reverts commit 7d85d965dc33783166a10bbe579d73506938883d.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    state = { lat: null, errorMessage: '' }
+    constructor(props) {
+        // must call super() in constructor
+        super(props);
 
-    componentDidMount() {
+        // this is only time, we direct assign object to this.state
+        // elseWhere, we use setState()
+        this.state = { lat: null, errorMessage: '' };
+
         window.navigator.geolocation.getCurrentPosition(
             (position) => this.setState({ lat: position.coords.latitude }),
             (err) => this.setState({ errorMessage: err.message })
