3f8ee3102f52fa76f026822564ae680d56d67ee8
umarsadiqi <82892724+umarsadiqi@users.noreply.github.com>
2022-02-13 13:17:13 +0500

    onChange={() => ()}
    Another approach to add event handler

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,10 +3,10 @@ import SearchBar from "./SearchBar";
 
 const App = () => {
     return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
+        <div className="ui container" style={{ marginTop: "10px" }}>
             <SearchBar />
         </div>
     );
-}
+};
 
 export default App;
