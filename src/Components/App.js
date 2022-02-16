91f00a79b00855b0b20d73adf5ae343bd436d37f
umarsadiqi <82892724+umarsadiqi@users.noreply.github.com>
2022-02-17 02:16:22 +0500

    Fetching data
    ⨀ using axios,
        → axios.get('', {})

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get("https://api.unsplash.com//search/photos", {
+            params: { query: term },
+            headers: {
+                Authorization:
+                    "Client-ID fQG59eCcttGsymPIuGsl6ORbiGN9xqV2Zvp60npJQWU",
+            },
+        });
     }
 
     render() {
