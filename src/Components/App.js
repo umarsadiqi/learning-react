9c6141c58d3a6b87258d41a98dc743493a090002
umarsadiqi <82892724+umarsadiqi@users.noreply.github.com>
2022-02-17 04:07:49 +0500

    code clean for api - request client
    ⨀ axios.create

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,14 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     state = { images: [] };
 
     onSearchSubmit = async (term) => {
-        const response = await axios.get(
+        const response = await unsplash.get(
             "https://api.unsplash.com//search/photos",
-            {
-                params: { query: term },
-                headers: {
-                    Authorization:
-                        "Client-ID fQG59eCcttGsymPIuGsl6ORbiGN9xqV2Zvp60npJQWU",
-                },
-            }
+            { params: { query: term } }
         );
 
         this.setState({ images: response.data.results });
