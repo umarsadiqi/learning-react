022c4fb78991899854f96a463322658880be76e3
umarsadiqi <82892724+umarsadiqi@users.noreply.github.com>
2022-02-20 13:28:24 +0500

    Added keys to root element
    ⨀ also used object in map() parameter

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = (props) => {
-    const images = props.images.map((image) => {
-        return <img src={image.urls.small_s3} alt={image.alt_description} />;
+    const images = props.images.map(({ id, urls, alt_description }) => {
+        return <img key={id} src={urls.small_s3} alt={alt_description} />;
     });
 
     return <div>{images}</div>;
